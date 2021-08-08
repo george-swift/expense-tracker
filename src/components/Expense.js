8154758c6097224c2b0b94f89f7d240b4cf24ea6
George <71434803+george-swift@users.noreply.github.com>
2021-08-08 16:49:51 +0100

    Add Expense component for each expense

---
+++ src/components/Expense.js
@@ -0,0 +1,80 @@
+import PropTypes from 'prop-types';
+import { FaPencilAlt } from 'react-icons/fa';
+import { useFormState } from '../hooks';
+import Form from './ExpenseForm';
+
+const Expense = ({
+  id, title, amount, date, notes, onUpdate, onDelete,
+}) => {
+  const {
+    state, handleChange, visible, toggleDisplay, reset,
+  } = useFormState({
+    title, amount, date, notes,
+  });
+
+  const edit = () => {
+    if (visible) {
+      toggleDisplay();
+      reset();
+    } else {
+      toggleDisplay();
+    }
+  };
+
+  const update = (e) => {
+    e.preventDefault();
+    onUpdate(id, state);
+    toggleDisplay();
+  };
+
+  return (
+    <li className="expense">
+      <div className="details">
+        <p>
+          <span className="fw-bold fs-3">{`$${amount}`}</span>
+          <br />
+          <small>{date}</small>
+        </p>
+        <p className="align-self-center ms-3 flex-grow-1">
+          <span className="title">{title}</span>
+          <br />
+          <span>{notes || '...'}</span>
+        </p>
+        <button className="mb-1 edit" type="button" onClick={edit}>
+          <FaPencilAlt />
+        </button>
+      </div>
+      {visible && (
+        <div className="mt-3">
+          <hr />
+          <Form
+            id={id}
+            title={state.title}
+            amount={state.amount}
+            date={state.date}
+            notes={state.notes || ''}
+            handleChange={handleChange}
+            deleteAction={onDelete}
+            submitAction={update}
+          />
+        </div>
+      )}
+    </li>
+  );
+};
+
+Expense.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
+  date: PropTypes.string.isRequired,
+  notes: PropTypes.string,
+  onUpdate: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
+
+Expense.defaultProps = {
+  notes: '',
+};
+
+export default Expense;
