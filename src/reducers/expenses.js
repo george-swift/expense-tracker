07d4ec4ee36a955efce3bbb149685605b12c71db
George <71434803+george-swift@users.noreply.github.com>
2021-08-08 17:40:50 +0100

    Add expenses reducer

---
+++ src/reducers/expenses.js
@@ -0,0 +1,34 @@
+import {
+  FETCH_EXPENSES_SUCCESSFUL, CREATE_EXPENSE_SUCCESSFUL,
+  UPDATE_EXPENSE_SUCCESSFUL, DELETE_EXPENSE_SUCCESSFUL,
+  SIGN_OUT_SUCCESSFUL,
+} from '../constants';
+
+export default function expenses(state = [], action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case FETCH_EXPENSES_SUCCESSFUL:
+      return [...payload];
+
+    case CREATE_EXPENSE_SUCCESSFUL:
+      return [...state, payload];
+
+    case UPDATE_EXPENSE_SUCCESSFUL:
+      return [
+        ...state.map((expense) => (
+          expense.id === payload.id
+            ? { ...expense, ...payload }
+            : expense)),
+      ];
+
+    case DELETE_EXPENSE_SUCCESSFUL:
+      return state.filter((expense) => expense.id !== payload);
+
+    case SIGN_OUT_SUCCESSFUL:
+      return [];
+
+    default:
+      return state;
+  }
+}
