afe186453a001982722ada2c8f4e8401d61544ad
George <71434803+george-swift@users.noreply.github.com>
2021-10-19 04:33:59 +0100

    Refactor root saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,37 +1,29 @@
 import { runSaga } from 'redux-saga';
 import { takeEvery, takeLatest } from '@redux-saga/core/effects';
-
 import {
   signUp, signIn, editAccount, signOut,
 } from './accountSagas';
-
+import fetchReports from './reportSaga';
+import { createList, updateList, deleteList } from './listSagas';
 import {
-  fetchLists, createList, updateList, deleteList,
-} from './listSagas';
-
-import {
-  fetchExpenses, createExpense,
-  updateExpense, deleteExpense,
-  showExpenditure,
+  fetchExpenses, createExpense, updateExpense, deleteExpense,
 } from './expenseSagas';
-
 import {
-  SIGN_UP_REQUEST, LOG_IN_REQUEST, EDIT_USER_REQUEST,
-  USER_AUTHENTICATED, CREATE_LIST, UPDATE_LIST, DELETE_LIST,
+  SIGN_UP, LOG_IN, EDIT_USER, AUTHENTICATE_USER,
+  CREATE_LIST, UPDATE_LIST, DELETE_LIST,
   FETCH_EXPENSES, CREATE_EXPENSE, UPDATE_EXPENSE, DELETE_EXPENSE,
-  SHOW_EXPENDITURE, SIGN_OUT_REQUEST,
+  SIGN_OUT,
 } from '../constants';
 
 export default function* rootSaga() {
-  yield takeEvery(SIGN_UP_REQUEST, signUp);
-  yield takeEvery(LOG_IN_REQUEST, signIn);
-  yield takeLatest(EDIT_USER_REQUEST, editAccount);
-  yield takeLatest(SIGN_OUT_REQUEST, signOut);
-  yield takeLatest(USER_AUTHENTICATED, fetchLists);
+  yield takeEvery(SIGN_UP, signUp);
+  yield takeEvery(LOG_IN, signIn);
+  yield takeLatest(AUTHENTICATE_USER, fetchReports);
+  yield takeLatest(EDIT_USER, editAccount);
+  yield takeLatest(SIGN_OUT, signOut);
   yield takeEvery(CREATE_LIST, createList);
   yield takeEvery(UPDATE_LIST, updateList);
   yield takeLatest(DELETE_LIST, deleteList);
-  yield takeLatest(SHOW_EXPENDITURE, showExpenditure);
   yield takeLatest(FETCH_EXPENSES, fetchExpenses);
   yield takeEvery(CREATE_EXPENSE, createExpense);
   yield takeEvery(UPDATE_EXPENSE, updateExpense);
