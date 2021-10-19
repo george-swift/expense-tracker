db0c6cfbbd2c98cfa301d433a8b26f0ac144cfaf
George <71434803+george-swift@users.noreply.github.com>
2021-10-19 04:41:43 +0100

    Add created actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,34 +1,72 @@
+import { createAction } from '@reduxjs/toolkit';
 import {
-  CLEAR_NOTIFICATIONS, SIGN_UP_REQUEST, LOG_IN_REQUEST,
-  USER_AUTHENTICATED, EDIT_USER_REQUEST, SIGN_OUT_REQUEST,
+  SIGN_UP, LOG_IN, AUTHENTICATE_USER, EDIT_USER, SIGN_OUT,
   CREATE_LIST, UPDATE_LIST, DELETE_LIST, FETCH_EXPENSES,
-  CREATE_EXPENSE, UPDATE_EXPENSE, SHOW_EXPENDITURE, DELETE_EXPENSE,
+  CREATE_EXPENSE, UPDATE_EXPENSE, DELETE_EXPENSE,
+  FETCH_REPORT, REQUEST_FAILED, CLEAR_NOTIFICATIONS,
 } from '../constants';
 
-export const signUpRequest = (data) => ({ type: SIGN_UP_REQUEST, payload: { user: data } });
+export const signUpRequest = createAction(SIGN_UP, (data) => ({ payload: { user: data } }));
 
-export const logInRequest = (data) => ({ type: LOG_IN_REQUEST, payload: data });
+export const logInRequest = createAction(LOG_IN, (data) => ({ payload: data }));
 
-export const authUser = (data) => ({ type: USER_AUTHENTICATED, payload: data });
+export const authUser = createAction(AUTHENTICATE_USER, (data) => ({ payload: data }));
 
-export const editUserRequest = (id, data) => ({ type: EDIT_USER_REQUEST, payload: { id, data } });
+export const editUserRequest = createAction(EDIT_USER, (id, data) => (
+  {
+    payload: {
+      id,
+      data,
+    },
+  }
+));
 
-export const signOutRequest = () => ({ type: SIGN_OUT_REQUEST });
+export const signOutRequest = createAction(SIGN_OUT);
 
-export const clearFlash = () => ({ type: CLEAR_NOTIFICATIONS });
+export const clearNotifications = createAction(CLEAR_NOTIFICATIONS);
 
-export const showExpenditure = (id) => ({ type: SHOW_EXPENDITURE, payload: id });
+export const fetchReport = createAction(FETCH_REPORT, (id) => ({ payload: id }));
 
-export const createList = (id, data) => ({ type: CREATE_LIST, payload: { id, data } });
+export const createList = createAction(CREATE_LIST, (id, data) => (
+  {
+    payload: {
+      id,
+      data,
+    },
+  }
+));
 
-export const updateList = (id, data) => ({ type: UPDATE_LIST, payload: { id, data } });
+export const updateList = createAction(UPDATE_LIST, (id, data) => (
+  {
+    payload: {
+      id,
+      data,
+    },
+  }
+));
 
-export const deleteList = (id) => ({ type: DELETE_LIST, payload: id });
+export const deleteList = createAction(DELETE_LIST, (id) => ({ payload: id }));
 
-export const fetchExpenses = (id) => ({ type: FETCH_EXPENSES, payload: id });
+export const fetchExpenses = createAction(FETCH_EXPENSES, (id) => ({ payload: id }));
 
-export const createExpense = (id, data) => ({ type: CREATE_EXPENSE, payload: { id, data } });
+export const createExpense = createAction(CREATE_EXPENSE, (id, data) => (
+  {
+    payload: {
+      id,
+      data,
+    },
+  }
+));
 
-export const updateExpense = (id, data) => ({ type: UPDATE_EXPENSE, payload: { id, data } });
+export const updateExpense = createAction(UPDATE_EXPENSE, (id, data) => (
+  {
+    payload: {
+      id,
+      data,
+    },
+  }
+));
 
-export const deleteExpense = (id) => ({ type: DELETE_EXPENSE, payload: id });
+export const deleteExpense = createAction(DELETE_EXPENSE, (id) => ({ payload: id }));
+
+export const requestFailed = createAction(REQUEST_FAILED, (message) => ({ payload: message }));
