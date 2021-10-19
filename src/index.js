5c73cd34fe8d9b6f5f1163acad9df5ce24a29b40
George <71434803+george-swift@users.noreply.github.com>
2021-10-19 04:42:19 +0100

    Refactor named imports

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import createSagaMiddleware from '@redux-saga/core';
 import { Provider } from 'react-redux';
-import rootReducer from './reducers';
-import rootSaga from './sagas';
 import App from './components/App';
-
-const sagaMiddleware = createSagaMiddleware();
-
-const store = createStore(rootReducer, {}, applyMiddleware(sagaMiddleware));
-
-sagaMiddleware.run(rootSaga);
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
