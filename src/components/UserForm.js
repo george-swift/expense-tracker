70b43dd2dc7f951864087c500c407fdd07b8a258
George <71434803+george-swift@users.noreply.github.com>
2022-01-04 02:29:24 +0100

    Import Button component

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/button';
+import Button from 'react-bootstrap/Button';
 
 const UserForm = ({
   auth,
