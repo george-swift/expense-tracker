6a0e04472247ae5407a3c9eb69c47e6310773c30
George <71434803+george-swift@users.noreply.github.com>
2022-03-25 22:00:59 +0100

    Wrap nested routes in Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,33 +1,20 @@
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import Navbar from './Nav';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './Navigation';
 import Lists from '../containers/Lists';
 import Tracker from '../containers/Tracker';
 import Reports from '../containers/Reports';
-import More from '../containers/More';
+import Profile from '../containers/Profile';
 
-export default function Main() {
-  const navigate = useNavigate();
-  const authed = useSelector((state) => state.user.authenticated);
+const Main = () => (
+  <div className="wrapper">
+    <Navigation />
+    <Routes>
+      <Route index element={<Lists />} />
+      <Route path="/track" element={<Tracker />} />
+      <Route path="/reports" element={<Reports />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </div>
+);
 
-  const [show, setShow] = useState(false);
-  const handleShow = () => setShow(true);
-  const handleClose = () => setShow(false);
-
-  useEffect(() => {
-    if (authed === false) navigate('/', { replace: true });
-  }, [authed]);
-
-  return (
-    <div className="vh-100 position-relative">
-      <Routes>
-        <Route path="/" element={<Lists />} />
-        <Route path="/track" element={<Tracker />} />
-        <Route path="/reports" element={<Reports />} />
-        <Route path="/more" element={<More />} />
-      </Routes>
-      <Navbar show={show} handleShow={handleShow} handleClose={handleClose} />
-    </div>
-  );
-}
+export default Main;
